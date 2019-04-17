e6c8b9e08c41f779fe45f2af1c9ed50cf2650b1e
Andres Donoso <af.donoso@uniandes.edu.co>
2019-04-16 23:31:07 -0500

    Modificado

--- iteracion1/docs/Sentencias SQL/Tablas/TablaProductosConsumo.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE ProductosConsumo(
-    id_consumo number(20) NOT NULL,
-    id_producto number(20) NOT NULL,
-    PRIMARY KEY(id_consumo, id_producto),
-    FOREIGN KEY(id_consumo) REFERENCES Consumo(id),
-    FOREIGN KEY(id_producto) REFERENCES Producto(id)
-);
