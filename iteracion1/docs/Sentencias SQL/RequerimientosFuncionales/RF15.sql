d1ad889cc45a346e7ccbe7e51a6d46fa7130d456
Andres Donoso <af.donoso@uniandes.edu.co>
2019-04-17 19:47:56 -0500

    Create RF15.sql

---
+++ iteracion1/docs/Sentencias SQL/RequerimientosFuncionales/RF15.sql
@@ -0,0 +1,20 @@
+/* Se crea el mantenimiento */
+INSERT INTO Mantenimiento
+VALUES (1, '23/10/2019', '25/10/2019');
+
+/* Se enlistan los servicios y/o habitaciones involucradas */
+
+INSERT INTO MantenimientoHabitacion
+VALUES (1, '103');
+
+INSERT INTO MantenimientoHabitacion
+VALUES (1, '108');
+
+INSERT INTO MantenimientoHabitacion
+VALUES(1, '109');
+
+INSERT INTO MantenimientoServicio
+VALUES (1, 2);
+
+INSERT INTO MantenimientoServicio
+VALUES (1, 9);
